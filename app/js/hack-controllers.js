404227d675980958196ded2fdcf5ea6a830cc96a
alex <alexkao7@gmail.com>
2014-11-22 10:38:17 -0500

    Added new impact data

--- app/js/hack-controllers.js
+++ app/js/hack-controllers.js
@@ -1,25 +1,27 @@
 phonecatControllers.controller('HackCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
 
-    $http.get('resources/batch-failed-data.json').success(function(data) {
-        $scope.batchFailedData = data;
 
-        var arrayToDisplay = [];
-        arrayToDisplay.push(["Date", "Failed"]);
-        var length = $scope.batchFailedData.length;
-        for (var i=0; i<length; i++) {
-            var batch = $scope.batchFailedData[i];
-            var arrayEntry = [batch.Batch.date, parseInt(batch.Batch.numFailed)];
-            arrayToDisplay.push(arrayEntry);
-        }
+    $http.get('resources/impact-data.json').success(function(data) {
+        console.log("Was able to load");
+        $scope.impactData = data;
 
-        var options = {
-          title: 'Batch Failed'
-        };
+        // var arrayToDisplay = [];
+        // arrayToDisplay.push(["Date", "Failed"]);
+        // var length = $scope.impactData.length;
+        // for (var i=0; i<length; i++) {
+        //     var batch = $scope.impactData[i];
+        //     var arrayEntry = [batch.Batch.date, parseInt(batch.Batch.numFailed)];
+        //     arrayToDisplay.push(arrayEntry);
+        // }
+
+        // var options = {
+        //   title: 'Batch Failed'
+        // };
         
-        var dataToDisplay = google.visualization.arrayToDataTable(arrayToDisplay);
-        var chart = new google.visualization.ColumnChart(document.getElementById('chartdiv'));
-        chart.draw(dataToDisplay, options);
+        // var dataToDisplay = google.visualization.arrayToDataTable(arrayToDisplay);
+        // var chart = new google.visualization.ColumnChart(document.getElementById('chartdiv'));
+        // chart.draw(dataToDisplay, options);
 
     });
 
