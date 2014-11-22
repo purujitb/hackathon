e8e638671f84e1541604889d8cad9a093a271b18
alex <alexkao7@gmail.com>
2014-11-22 12:16:04 -0500

    added side bar

--- app/js/hack-controllers.js
+++ app/js/hack-controllers.js
@@ -6,6 +6,50 @@ phonecatControllers.controller('HackCtrl', ['$scope', '$routeParams', '$http',
         console.log("Was able to load");
         $scope.impactData = data;
 
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+          ]);
+
+        var options = {
+          title: 'My Daily Activities'
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+      chart.draw(data, options);
+
+
+
+      var barData = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+          ]);
+
+      var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+        },
+          bars: 'horizontal' // Required for Material Bar Charts.
+      };
+
+      var barchart = new google.visualization.BarChart(document.getElementById('barchart'));
+
+      barchart.draw(barData, options);
+
+
+
+
+
         // var arrayToDisplay = [];
         // arrayToDisplay.push(["Date", "Failed"]);
         // var length = $scope.impactData.length;
@@ -25,4 +69,4 @@ phonecatControllers.controller('HackCtrl', ['$scope', '$routeParams', '$http',
 
     });
 
-  }]);
+}]);
